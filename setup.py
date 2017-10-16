696d0aad281e27af7aae0f4a76a9ba3ef50b24ed
Yacin Tmimi <yacintmimi@gmail.com>
2017-10-16 13:27:33 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='ma_option_vol',
-      version='1.0',
-      description='Code to help with the collection and analysis of data for my Fall 2017 Grossman School of Business Honors Thesis',
+      version='1.0.2',
+      
+      #project desctiption
+      description='Thesis Code',
+      long_description='Code to help with the collection and analysis of data for my Fall 2017 Grossman School of Business Honors Thesis',
+
+      #author info
       author='Yacin Tmimi',
       author_email='ytmimi@uvm.edu',
+
+      #homepage for the project
       url='https://github.com/ytmimi/Thesis2017',
-      download_url='https://github.com/ytmimi/Thesis2017',
+
+      #open source license
       license='MIT',
 
-      packages=['ma_option_vol'],
-      data_files=[('company_data/sample',['M&A List A-S&P500 T-US Sample Set.xlsx'])]
+      #local packages to be installed
+      packages=['ma_option_vol', 'company_data'],
+
+      #python modules that the code needs to run properly
+      install_requires=['openpyxl'],
+      
+      #non python files to be included with the source distribution
+      package_data={
+        'company_data': ['sample/*.xlsx'],
+        '': ['LICENSE.txt','README.md'],
+      },
      )
