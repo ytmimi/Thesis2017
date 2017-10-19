4d6fb5037e11b6132a799eb9697d6ad12ef20f65
Yacin Tmimi <yacintmimi@gmail.com>
2017-10-18 21:11:19 -0400

    worked on finished update_option_contract_tab()

--- ma_option_vol/update_excel_workbooks.py
+++ ma_option_vol/update_excel_workbooks.py
@@ -1,4 +1,5 @@
 import openpyxl
+import datetime as dt
 import add_bloomberg_excel_functions as abxl
 
 def update_sheet_with_BDP_description(workbook_path, sheet_name):
@@ -19,92 +20,155 @@ def update_sheet_with_BDP_description(workbook_path, sheet_name):
     #saves the workbook
     #wb.save(workbook_path) <----uncomment this after testing
 
-def update_option_contract_tabs(workbook_path, sheet_name):
+def update_option_contract_tabs(workbook_path, sheet_name, sheet_end_date_cell, test=False):
     '''
-    
+    Creates new sheets in the given excel workbook based on Option data stored in the given sheet.
     '''
+    #data labels to be added to the new excel worksheet
+    option_data_labels = ['Security Name', 'Description', 'Type', 'Expiration Date', 'Strike Price']
+
+    #headers for the datatable to be added to the new worksheet
+    data_table_header = ['INDEX','DATE','PX_LAST','PX_BID','PX_ASK','PX_VOLUME','OPEN_INT', 'IVOL']
+
+
+    #given the full_file path an excel workbook is loaded.
     wb = openpyxl.load_workbook(workbook_path)
-    #The active sheet is the first sheet, which in this case is "Options Chain"
-    sheet = wb.get_sheet_by_name(sheet_name)
     
+    #The sheet we want to get data from is set to the variable sheet
+    data_sheet = wb.get_sheet_by_name(sheet_name)
     
-    #if there are already tabs get rid of the ones we don't want:
-    if len(wb.get_sheet_names()) >1:
-           for i,x in enumerate(wb.get_sheet_names()):
-                if i > 0:
-                    wb.remove_sheet(wb.get_sheet_by_name(x))
+    #The cell in the sheet that contains the completion/termination date
+    completion_date= data_sheet[sheet_end_date_cell].value.date()
 
-    # now add the sheets that we want
+    #if we're running a test, do the following:
+    if test:
+        #if there are already tabs get rid of the ones we don't want:
+        print(len(wb.get_sheet_names()))
+        if len(wb.get_sheet_names()) >1:
+               for i,x in enumerate(wb.get_sheet_names()):
+                    if i > 0:
+                        wb.remove_sheet(wb.get_sheet_by_name(x))
+
+
+    total_rows = data_sheet.max_row
+    #iterate through the rows of the data_sheet
     #NOTE: THE SHEET IS SET UP SO THAT VALUES WE'RE INTERESTED IN START AT ROW 10
-    for i in range(10, sheet.max_row+1):
-    #for (index, cell) in enumerate(sheet['A10:B{}'.format(sheet.max_row)]):
+    for (index, cell) in enumerate(data_sheet['A10:B{}'.format(total_rows)]):
         
-        new_sheet = wb.create_sheet()
-        
-        option_description = sheet['B{}'.format(i)].value
-        
-        #just in case we weren't able to find options, the tab can still be created
-        if option_description == None:
-            option_description = 'No Options'
-        security_name = sheet['A{}'.format(i)].value
-      
-        # '/' aren't allowed in excel tab names, so we replace them with '-' 
-
-        tab_name = option_description.replace('/', '-')
-
-        #will split the option_description into a list that looks like: ['PFE', 'US', '12/20/14', 'P18']
-        #This information will be used to give values to the labels set up below
-        description_list = option_description.split(' ')
-
-
-        #Setting the labels for the sheet
-        new_sheet.title =  tab_name
-        new_sheet['A1'] ='Security Name'
-        new_sheet['A2'] ='Description'
-        new_sheet['A3'] = 'Type'
-        new_sheet['A4'] = 'Expiration Date'
-        new_sheet['A5'] = 'Strike'
-
-        #Setting the values for the labels
-        new_sheet['B1'] = security_name
-        new_sheet['B2'] = option_description
-        if description_list[-1][0] =='P':
-            new_sheet['B3'] = 'Put'
-        elif description_list[-1][0] == 'C':
-            new_sheet['B3'] = 'Call'
-        else:
-            new_sheet['B3'] = '?'
-        
-        #we converted option_descrption to a string earlier, so we have to compare option_description to the string 'None'
-        if option_description == 'No Options':
+        #if there is no option description, then break out of this loop
+        if cell[1].value == None:
+            print('No option description found. Could not create new workbook sheets')
             break
+
+        #format_option_data() returns the following list:
+        #[security_name, option_description, option_type, expiration_date, strike_price]
+        option_data = format_option_data(cell[0].value, cell[1].value)
+
+        #the number of days between the expiration and completion date. 
+        date_diff = (option_data[3] - completion_date).days
+
+        #if the expiration_date occurs 2 months after the completion_date, then stop creating sheets
+        if date_diff >= 60:
+            print('Found contract past {}. Saving the workbook with {} new tabs'.format(option_data[3], index-1))
+            print('Potential new tabs: {}'.format(total_rows-10))
+            wb.save(workbook_path)
+            break
+
+        #otherwise, keep creating sheets
         else:
-            new_sheet['B4'] = description_list[2]
-            new_sheet['B5'] = description_list[-1][1:]
+            #creates a new sheet for the passed in workbook
+            new_sheet = wb.create_sheet()
+            #/' aren't allowed in excel sheet names, so we replace them with '-' 
+            new_sheet.title = option_data[1].replace('/', '-')
 
-        data_table_header = ['INDEX','DATE','PX_LAST','PX_BID','PX_ASK','PX_VOLUME','OPEN_INT', 'IVOL']
-        #ADDING DATA COLUMN LABELS:
-        new_sheet['A8'] = 'INDEX'
-        new_sheet['B8'] = 'DATE'
-        new_sheet['C8'] = 'PX_LAST'
-        new_sheet['D8'] = 'PX_BID'
-        new_sheet['E8'] = 'PX_ASK'
-        new_sheet['F8'] = 'PX_VOLUME'
-        new_sheet['G8'] = 'OPEN_INT'
-        new_sheet['H8'] = 'IVOL'
+            #zip creates a tuple for each item of the passed in lists. this tuple can then be appended to the sheet
+            for data in zip(option_data_labels,option_data):
+                new_sheet.append(data)
 
-        #add the BDH formula to cell B9
-        new_sheet['B9'] = abxl.add_option_BDH(  security_name = 'B1',
-                                                fields = 'C8:H8', 
-                                                start_date = "'Options Chain'!B4",
-                                                end_date = "'Options Chain'!B6",
-                                                optional_arg = '"Days, Fill"',
-                                                optional_val = '"W,  0"')
+            #loop through every value of the data_table_header and add it to the worksheet A8:H8
+            for index, value in enumerate(data_table_header, start= 1) :
+                new_sheet.cell(row = 8,column = index ).value = value 
+
+            #add the BDH formula to cell B9
+            new_sheet['B9'] = abxl.add_option_BDH(  security_name = 'B1',
+                                                    fields = 'C8:H8', 
+                                                    start_date = "'Options Chain'!B4",
+                                                    end_date = "'Options Chain'!B6",
+                                                    optional_arg = ['Days', 'Fill'],
+                                                    optional_val = ['W',  '0'])
+            
+    #if the loop ends without finding contracts 2 months past the completion/termination date, save the workbook      
+    wb.save(workbook_path)  
+                                                    
+    #     new_sheet['A1'] ='Security Name'
+    #     new_sheet['A2'] ='Description'
+    #     new_sheet['A3'] = 'Type'
+    #     new_sheet['A4'] = 'Expiration Date'
+    #     new_sheet['A5'] = 'Strike'
+
+    #     #Setting the values for the labels
+    #     new_sheet['B1'] = security_name
+    #     new_sheet['B2'] = option_description
+    #     if description_list[-1][0] =='P':
+    #         new_sheet['B3'] = 'Put'
+    #     elif description_list[-1][0] == 'C':
+    #         new_sheet['B3'] = 'Call'
+        
+    #     #we converted option_descrption to a string earlier, so we have to compare option_description to the string 'None'
+    #     if option_description == 'No Options':
+    #         break
+    #     else:
+    #         new_sheet['B4'] = description_list[2]
+    #         new_sheet['B5'] = description_list[-1][1:]
+
+    #     data_table_header = ['INDEX','DATE','PX_LAST','PX_BID','PX_ASK','PX_VOLUME','OPEN_INT', 'IVOL']
+    #     #ADDING DATA COLUMN LABELS:
+    #     new_sheet['A8'] = 'INDEX'
+    #     new_sheet['B8'] = 'DATE'
+    #     new_sheet['C8'] = 'PX_LAST'
+    #     new_sheet['D8'] = 'PX_BID'
+    #     new_sheet['E8'] = 'PX_ASK'
+    #     new_sheet['F8'] = 'PX_VOLUME'
+    #     new_sheet['G8'] = 'OPEN_INT'
+    #     new_sheet['H8'] = 'IVOL'
+
+    #     #add the BDH formula to cell B9
+    #     new_sheet['B9'] = abxl.add_option_BDH(  security_name = 'B1',
+    #                                             fields = 'C8:H8', 
+    #                                             start_date = "'Options Chain'!B4",
+    #                                             end_date = "'Options Chain'!B6",
+    #                                             optional_arg = '"Days, Fill"',
+    #                                             optional_val = '"W,  0"')
 
 
-        #Get rid of break after done testing:
-        #wb.save(file_path) <-------------------remove after done testing
-    print('Done')
+    #     #Get rid of break after done testing:
+    #     #wb.save(file_path) <-------------------remove after done testing
+    # print('Done')
+
+def format_option_data(security_name, option_description):
+    '''
+    security_name should be a string that looks similar to 'BBG00673J6L5 Equity'
+    option_description should be a string that looks similar to 'PFE US 12/20/14 P18'
+    given the security_name and option_description a list of data will be output
+    '''
+    #will split the option_description by whitespace into a list that looks like: ['PFE', 'US', '12/20/14', 'P18']
+    description_list = option_description.split(' ')
+
+    #determins the option type based on description_list[-1][0] = 'P'
+    if description_list[-1][0] =='P':
+        option_type = 'Put'
+    elif description_list[-1][0] == 'C':
+        option_type = 'Call'
+
+    #description_list[2] = 12/20/14 and convertis it into a datetime object
+    expiration_date = dt.datetime.strptime(description_list[2],'%m/%d/%y').date()
+
+    #description_list[-1][1:] = '18', and converts the string to an int
+    strike_price = description_list[-1][1:]
+
+    option_data_list = [security_name, option_description, option_type, expiration_date, strike_price]
+
+    return option_data_list
 
 
 def update_index(file_path):
