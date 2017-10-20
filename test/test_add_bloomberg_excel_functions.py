c2fa3081186e77b1025074e0589b457d53237d12
Yacin Tmimi <yacintmimi@gmail.com>
2017-10-20 02:35:03 -0400

    minor changes to test files

--- test/test_add_bloomberg_excel_functions.py
+++ test/test_add_bloomberg_excel_functions.py
@@ -9,22 +9,75 @@ path = os.path.join(parent_path,'ma_option_vol')
 sys.path.append(path)
 
 #imports the add_bloomber_excel_functions module
-from ma_option_vol import add_bloomberg_excel_functions as abxl
+#imported from one line becasue its path is already in sys.path
+import add_bloomberg_excel_functions as abxl
 
-
+	
 '''
 add_BDS_OPT_CHAIN() EXAMPLE
 
-creates a string representing the Bloomberg BDS function for options chains to be inserted into an excel WorkSheets Cell
-Note: the document needs to be run in order for the formulas to be calculated
+Creates a string representing the Bloomberg BDS function for options chains to be used in an excel worksheet.
+Note: the excel document needs to be run in order for the formulas to be calculated
 
 Example: add_BDS_OPT_CHAIN(ticker_cell='B2',type_cell='B3', date_override_cell='B6')
-         Gets values from the cell cooridinates passed to the function
+         function arguments are string cell references
          
 Example: add_BDS_OPT_CHAIN(ticker_cell='AAPL US',type_cell='EQUITY', date_override_cell='20151231')
-         Gets data from bloomberg based on the literal commands excepted by the BDS function
+         function arguments are literal string commands excepted by the BDS function
 '''
 
+#test the function with both cell references and valid Bloomberg strings
+print('\n')
 print(abxl.add_BDS_OPT_CHAIN(ticker_cell='B2',type_cell='B3', date_override_cell='B6'))
 print(abxl.add_BDS_OPT_CHAIN(ticker_cell='AAPL US',type_cell='EQUITY', date_override_cell='20151231'))
+
+
+'''
+add_BDP_fuction() EXAMPLE
+
+Creates a string representing the Bloomberg BDP function to be used in an excel worksheet.
+Note: the excel document needs to be run in order for the formulas to be calculated
+
+Example: add_BDP_fuction(security_cell='A3', field_cell='A4')
+		 function arguments are string cell references
+
+Example: add_BDP_fuction(security_cell='BBG00673J6L5 Equity', field_cell='SECURITY_DES')
+    	 function arguments are literal string commands excepted by the BDS function
+'''
+#test the function with both cell references and valid Bloomberg strings
+print('\n')
+print(abxl.add_BDP_fuction(security_cell='A3', field_cell='A4'))
+print(abxl.add_BDP_fuction(security_cell='BBG00673J6L5 Equity', field_cell='SECURITY_DES'))
+print(abxl.add_BDP_fuction(security_cell='A3', field_cell='PX_LAST'))
+
+
+
+'''
+add_option_BDH() EXAMPLE
+
+Creates a string representing the Bloomberg BDH function to be used in an excel worksheet.
+Note: the excle document needs to be run in order for the formulas to be calculated
+
+Example: add_option_BDH(security_name='B4', fields='C3', start_date='A1', end_date='A2', optional_arg = None, optional_val=None)
+		 function arguments are string cell references 
+
+Example: add_option_BDH(security_name='B4', fields='C3:C5', start_date='A1', end_date='A2', optional_arg = 'E1:E2', optional_val='F1:F2')
+		 function arguments are a combination of cells and cell references
+
+Example: add_option_BDH(security_name='IBM US EQUITY', fields='PX_BID, PX_ASK, PX_VOLUME', start_date='20150520', end_date='20170915', optional_arg = 'Days', optional_val='W')
+		 function arguments are strings
+
+Example: add_option_BDH(security_name='B4', fields=['PX_LAST','PX_BID'], start_date='20131230', end_date='A2', optional_arg = 'Days, Fill', optional_val=['W','0'])
+		 function argumetns are strings, cell references and lists
+
+'''
+print('\n')
+print(abxl.add_option_BDH(security_name='B4', fields='C3', start_date='A1', end_date='A2', optional_arg = None, optional_val=None))
+print(abxl.add_option_BDH(security_name='B4', fields='C3:C5', start_date='A1', end_date='A2', optional_arg = 'E1:E2', optional_val='F1:F2'))
+print(abxl.add_option_BDH(security_name='IBM US EQUITY', fields='PX_BID, PX_ASK, PX_VOLUME', start_date='20150520', end_date='20170915', optional_arg = 'Days', optional_val='W'))
+print(abxl.add_option_BDH(security_name='B4', fields=['PX_LAST','PX_BID'], start_date='20131230', end_date='A2', optional_arg = 'Days, Fill', optional_val=['W','0']))
+
+
+
+
 
