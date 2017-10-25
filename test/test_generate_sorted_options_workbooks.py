3d350ec25514c68da47d61e4030677334bf3b8d9
Yacin Tmimi <yacintmimi@gmail.com>
2017-10-25 14:49:58 -0400

    added a test for create_sorted_workbooks()

--- test/test_generate_sorted_options_workbooks.py
+++ test/test_generate_sorted_options_workbooks.py
@@ -17,27 +17,27 @@ import generate_sorted_options_workbooks as gsow
 
 
 #tests the convert_to_numbers() function
-letter_list = ['A', 'e', 'p', 'B', 'Z', 'AB']
-num_list=gsow.convert_to_numbers(letter_list)
-print(num_list)
+# letter_list = ['A', 'e', 'p', 'B', 'Z', 'AB']
+# num_list=gsow.convert_to_numbers(letter_list)
+# print(num_list)
 
 
 #test the group_contracts_by_strik() function
-test_wb = openpyxl.load_workbook(test_path)
-contracts = gsow.group_contracts_by_strike(wb = test_wb)
-print(contracts['call'].keys())
-print(contracts['put'].keys())
-print('\n')
-#prints a sample of the list values stored in each key 
-for (index, key) in enumerate(contracts['call']):
-	if index > 4:
-		break
-	print(key, contracts['call'][key])
-print('\n')
-for (index, key) in enumerate(contracts['put']):
-	if index > 4:
-		break
-	print(key, contracts['put'][key])
+# test_wb = openpyxl.load_workbook(test_path)
+# contracts = gsow.group_contracts_by_strike(wb = test_wb)
+# print(contracts['call'].keys())
+# print(contracts['put'].keys())
+# print('\n')
+# #prints a sample of the list values stored in each key 
+# for (index, key) in enumerate(contracts['call']):
+# 	if index > 4:
+# 		break
+# 	print(key, contracts['call'][key])
+# print('\n')
+# for (index, key) in enumerate(contracts['put']):
+# 	if index > 4:
+# 		break
+# 	print(key, contracts['put'][key])
 
 
 #test the create_sorted_workbooks function.
@@ -46,7 +46,7 @@ for (index, key) in enumerate(contracts['put']):
 #this test also test the other function.
 gsow.create_sorted_workbooks(reference_wb_path= test_path2,
 						data_start_row= 8, 
-						data_column=['C'], 
+						data_column=['C','E'], 
 						index_column=['A','B'])
 
 
