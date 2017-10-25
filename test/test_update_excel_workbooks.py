364f3ed793fe5f7a9eb4772802b35d02a780c00c
Yacin Tmimi <yacintmimi@gmail.com>
2017-10-25 14:47:47 -0400

    added tests for find_column_index_by_header() and update_workbook_average_column()

--- test/test_update_excel_workbooks.py
+++ test/test_update_excel_workbooks.py
@@ -1,8 +1,9 @@
 #imports to get the files
 import os
 import sys
+import openpyxl
 import pandas as pd
-from test_path import test_path
+from test_path import test_path, test_path3
 
 parent_path = os.path.abspath(os.pardir)
 path = os.path.join(parent_path,'ma_option_vol')
@@ -14,8 +15,6 @@ sys.path.append(path)
 #imported from one line becasue its path is already in sys.path
 import update_excel_workbooks as uxlw
 
-
-
 #update_sheet with the BDP function
 #uxlw.update_sheet_with_BDP_description(workbook_path =test_path)
 
@@ -25,7 +24,15 @@ import update_excel_workbooks as uxlw
 #deletes all worksheets in the workbook except the first worksheet
 #uxlw.delet_workbook_sheets(test_path)
 
-#update the index for each sheet with data in the workbook
+#update the index for each sheet in relation to the announcement date
 #uxlw.update_workbook_data_index(workbook_path =test_path)
 
+#test the find_column_index_by_header() function
+# wb = openpyxl.load_workbook(test_path3)
+# data_dict= uxlw.find_column_index_by_header(reference_wb =wb, column_header='PX_LAST', header_row=8)
+# for index, key in enumerate(data_dict):
+# 	print(key, data_dict[key])
 
+
+#test the update_workbook_average_column() function
+uxlw.update_workbook_average_column(reference_wb_path = test_path3, column_header='PX_LAST', header_row=8, data_start_row=9)
