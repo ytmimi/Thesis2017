7356caa3b53f077e96accc79fd5685df8c9be00b
Yacin Tmimi <yacintmimi@gmail.com>
2017-11-08 22:34:23 -0500

    updated the generate_sorted_sheets() function, and removed unecessary imports

--- ma_option_vol/generate_sorted_options_workbooks.py
+++ ma_option_vol/generate_sorted_options_workbooks.py
@@ -1,15 +1,15 @@
 import openpyxl
 import os
 import re
-import datetime as dt
+from update_excel_workbooks import convert_to_numbers, copy_data
 from CONSTANTS import CALL_DESIGNATION_PATTERN, PUT_DESIGNATION_PATTERN
 
 
 def group_contracts_by_strike(reference_wb):
     '''
-    Given a workbook with many option contracs, those that have the same strike price are grouped together.
+    Given a workbook with many option contracs, sheet names that contain options with the same strike price are grouped together.
     Returns a dictionary with keys representing the strike and type of the option (call or put), and the value being a list of
-    sheet names corresponding to each option with the given strike.
+    sheet names corresponding to each option for the given strike.
     '''
     #a dictionary to store the sorted data
     options_contracts = {}
@@ -39,7 +39,6 @@ def group_contracts_by_strike(reference_wb):
 
     #finally return the options_contracts dictionary
     return options_contracts
-
 
 
 def group_contracts_by_expiration(reference_wb):
@@ -99,7 +98,6 @@ def create_sorted_sheet(new_workbook, reference_wb, new_sheet_title, reference_s
                           specifies which columns of data are of interest from the reference_sheet contained in
                           in reference_sheet_list
     
-    
     index_column          should be a list of numbers, where 1=column A, 2=column B, 3=column C, and so on.
                           specifies which columns from the reference_sheet_list[0], you want to use as your index's
                           NOTE: For accurate indexing, each sheet of data referenced should have the same index
@@ -112,18 +110,12 @@ def create_sorted_sheet(new_workbook, reference_wb, new_sheet_title, reference_s
         new_sheet.title = new_sheet_title 
     #else create an entirely new sheet and give it the apporpiate new_sheet_title
     else:
-         new_sheet = new_workbook.create_sheet(title = new_sheet_title )
+         new_sheet = new_workbook.create_sheet(title = new_sheet_title)
         
-    
-    #formats the data_column, and index_column in case they were imput as letters.
-    data_column = convert_to_numbers(data_column)
-    index_column = convert_to_numbers(index_column)
-
-
     #iterate over all the sheets in the reference_wb that were passed in reference_sheet_list
-    for (index,sheet_name) in enumerate(reference_sheet_list):  
+    for (i,sheet_name) in enumerate(reference_sheet_list):  
         #if its the first iteration, update the sheet with the index_colum and the data_column
-        if index ==0:
+        if i ==0:
             #load in the new worksheet.
             data_sheet = reference_wb.get_sheet_by_name(sheet_name)
             
@@ -131,7 +123,7 @@ def create_sorted_sheet(new_workbook, reference_wb, new_sheet_title, reference_s
             max_row = data_sheet.max_row
             
             #iterate over the columns and the rows that we're interested in
-            for (index,column_num) in enumerate(index_column+data_column):
+            for (j,column_num) in enumerate(index_column+data_column):
                 #find the max_column of the new_worksheet
                 max_col = new_sheet.max_column
                 
@@ -140,21 +132,10 @@ def create_sorted_sheet(new_workbook, reference_wb, new_sheet_title, reference_s
                     #the header is set at the current max_col+1, which data will go under
                     new_sheet.cell(row= data_start_row-1, column= max_col+1).value = sheet_name
                     
-                    
-                #iterate over row indexes from data_start_row to max_row+1
-                for i in range(data_start_row, max_row+1):
-                    #if its the first iteration of adding data to the sheet
-                    if index == 0:
-                        new_sheet.cell(row=i, column=max_col).value = data_sheet.cell(row= i, column= column_num).value
-                    
-                    else:
-                    	#if the value is a datetime object, then make sure the value of the cell is a formated date
-                    	if type(data_sheet.cell(row= i, column= column_num).value) == dt.datetime:
-                        	new_sheet.cell(row=i, column=max_col+1).value = data_sheet.cell(row= i, column= column_num).value.date()
-                    	else:
-                        	new_sheet.cell(row=i, column=max_col+1).value = data_sheet.cell(row= i, column= column_num).value
-
-                                
+                if j == 0:
+                    copy_data(reference_sheet=data_sheet, main_sheet=new_sheet,index_start_row=data_start_row, index_end_row=max_row, reference_data_column=column_num, main_data_column=max_col)
+                else:
+                    copy_data(reference_sheet=data_sheet, main_sheet=new_sheet,index_start_row=data_start_row, index_end_row=max_row, reference_data_column=column_num, main_data_column=max_col+1)
 
         #else: just grab the data_column    
         else:
@@ -164,8 +145,8 @@ def create_sorted_sheet(new_workbook, reference_wb, new_sheet_title, reference_s
             #get the max rows of the loaded worksheet
             max_row = data_sheet.max_row
             
-            #iterate over the columns and rows that we're interested in
-            for (index,column_num) in enumerate(data_column):
+            #iterate over the columns in the data_sheet that we're interested in
+            for (j,column_num) in enumerate(data_column):
                 #find the max_column of the new_worksheet
                 max_col = new_sheet.max_column
                 
@@ -173,18 +154,9 @@ def create_sorted_sheet(new_workbook, reference_wb, new_sheet_title, reference_s
                 if column_num == data_column[0]:
                     #the header is set at the current max_col+1, which data will go under
                     new_sheet.cell(row= data_start_row-1, column=max_col+1).value = sheet_name
-                
-                #iterate over row indexes from data_start_row to max_row+1
-                for i in range(data_start_row, max_row+1):
-                    new_sheet.cell(row=i, column=max_col+1).value = data_sheet.cell(row= i, column= column_num).value
 
-
-def convert_to_numbers(lst):
-    for (index, value) in enumerate(lst):
-        if type(value) == str:
-            lst[index] = openpyxl.utils.column_index_from_string(value)
-    return lst
-
+                copy_data(reference_sheet=data_sheet, main_sheet=new_sheet,index_start_row=data_start_row, index_end_row=max_row, reference_data_column=column_num, main_data_column=max_col+1)
+ 
 
 
 def create_sorted_workbooks(reference_wb_path, header_start_row, data_column, index_column, sort_by_strike=True, sort_by_expiration=False):
@@ -193,13 +165,17 @@ def create_sorted_workbooks(reference_wb_path, header_start_row, data_column, in
 
 	reference_wb_path   should be the path to the workbook that data will be taken from
 
-	header_start_row      is a variable that will be passed to the call of create_sorted_sheet()
+	header_start_row    is a variable that will be passed to the call of create_sorted_sheet()
 
 	data_column         is a variable that will be passed to the call of create_sorted_sheet()
 
 	index_column        is a variable that will be passed to the call of create_sorted_sheet()
+
+	sort_by_strike		A boolean that determins weather the new options workbook will be sorted by strike, defaults to True
+
+	sort_by_expiration	A boolean that determins weather the new options workbook will be sorted by expiration, defaults to False
 	'''
-	#initial check to ensure that the user has designated one sort method, but not both. By defualt sort_by_stike is active.
+	#initial check to ensure that the user has designated one sort method, but not both. By defualt sort_by_stike is True.
 	if(sort_by_strike and sort_by_expiration):
 		return print('Error: Choose either to sort by strike or by expiration')
 
@@ -209,23 +185,27 @@ def create_sorted_workbooks(reference_wb_path, header_start_row, data_column, in
 	#loads the reference workbook
 	reference_wb = openpyxl.load_workbook(reference_wb_path)
 
+	#formats the data_column, and index_column in case they were imput as letters.
+	data_column = convert_to_numbers(data_column)
+	index_column = convert_to_numbers(index_column)
+
 	#creats the new workbooks.
 	new_call_wb = openpyxl.Workbook()
 	new_put_wb = openpyxl.Workbook()
 
-	#adds the stock sheet to the new_call_wb.  This is called outside of the for loop
+	#adds the stock sheet to the new_call_wb.
 	create_sorted_sheet(new_workbook =new_call_wb, reference_wb= reference_wb,
 						new_sheet_title= 'Stock Price', reference_sheet_list=[reference_wb.get_sheet_names()[1]],
 						data_start_row= header_start_row, data_column= [data_column[0]],
 						index_column= index_column)
 
-	#adds the stock sheet to the new_put_wb.  This is called outside of the for loop
+	#adds the stock sheet to the new_put_wb.
 	create_sorted_sheet(new_workbook =new_put_wb, reference_wb= reference_wb,
 						new_sheet_title= 'Stock Price', reference_sheet_list=[reference_wb.get_sheet_names()[1]],
 						data_start_row= header_start_row, data_column= [data_column[0]],
 						index_column= index_column)
 
-	#sets the value of contracts based on the type of sort designated
+	#sets the value of the variable contracts based on the type of sort designated
 	if sort_by_strike:
 		#stores the outpuf of group_contracts_by_strike(), which is a nested dictionary that looks similar to:
 		#{ {'call':{'C55':['List of contract sheets'], ....}, {'put':{'P55':['List of contract sheets']} } } }
@@ -243,7 +223,6 @@ def create_sorted_workbooks(reference_wb_path, header_start_row, data_column, in
 							new_sheet_title= key, reference_sheet_list=contracts['call'][key],
 							data_start_row= header_start_row, data_column= data_column,
 							index_column= index_column)
-
 
 	#iterate over all the keys in the 'put' dictionary stored in contracts 
 	for (index, key) in enumerate(contracts['put']):
@@ -308,7 +287,9 @@ def save_new_workbook(new_workbook,workbook_path,new_folder,append_file_name):
         #save the workbook
         new_workbook.save(final_path)
         print('Saving {}.{}'.format(new_file_name,split_file_name[-1]))
- 
+
+
+
 
 
 
