c712b2b0647cd6d4ac08bfb7e8abc999a4a5802b
Yacin Tmimi <yacintmimi@gmail.com>
2017-12-09 16:02:13 -0500

    added OUTPUT_DIR, SAMPLE_DIR, AND MERGER_DIR

--- ma_option_vol/CONSTANTS.py
+++ ma_option_vol/CONSTANTS.py
@@ -12,10 +12,20 @@ ACQUIRER_DIR = os.path.join(ROOT_PATH,'company_data', 'acquirer')
 #path to the target folder
 TARGET_DIR = os.path.join(ROOT_PATH,'company_data', 'target')
 
+#path to the output folder
+OUTPUT_DIR = os.path.join(ROOT_PATH, 'company_data', 'function_output')
+
+#path to the sample folder
+SAMPLE_DIR = os.path.join(ROOT_PATH, 'company_data', 'sample')
+
+#sample file path
+MERGER_SAMPLE = os.path.join(SAMPLE_DIR,'M&A List A-S&P500 T-US Sample Set.xlsx')
+
+
 #a regular expression for a formated option description where the strike is an integer
 OPTION_SHEET_PATTERN_INT = re.compile(r'^\w+\s\w+\s\d{2}-\d{2}-\d{2}\s\w+$')
 
-#a regular expression for a formated option description where the strike is a foat
+#a regular expression for a formated option description where the strike is a float
 OPTION_SHEET_PATTERN_FLOAT= re.compile(r'^\w+\s\w+\s\d{2}-\d{2}-\d{2}\s\w+\.\w+$')
 
 #a regular expression pattern for the stock sheet
@@ -35,7 +45,7 @@ PUT_DESIGNATION_PATTERN = re.compile(r'[P]\d+')
 
 
 
-#USEFUL INFORMATINO FROM THE 'Treasury Rates.xlsx' file
+#usefull information for the treasury sheet
 TREASURY_WORKBOOK_PATH = '{}/{}/{}/{}'.format(os.path.abspath(os.pardir), 'company_data','sample', 'Week_Day Treasury Rates.xlsx')
 
 TREASURY_WORKSHEET= openpyxl.load_workbook(TREASURY_WORKBOOK_PATH, data_only=True).get_sheet_by_name('Rates')
