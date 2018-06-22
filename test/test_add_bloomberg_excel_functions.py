cd73a3c818ad74e7e4d7bbff46014a6ac5c8be1a
Yacin Tmimi <yacintmimi@gmail.com>
2018-06-22 06:52:08 -0400

    updated test to unittest framework

--- test/test_add_bloomberg_excel_functions.py
+++ test/test_add_bloomberg_excel_functions.py
@@ -1,17 +1,18 @@
 #imports to get the files
 import os
 import sys
+import unittest
+import datetime as dt
 
 parent_path = os.path.abspath(os.pardir)
 path = os.path.join(parent_path,'ma_option_vol')
 
-#adds the file path for the ma_options_vol module to the path that python will search in order to look for modules
+#adds the ma_options_vol module to the path that python searches in order to look for modules
 sys.path.append(path)
 
 #imports the add_bloomber_excel_functions module
 #imported from one line becasue its path is already in sys.path
 import add_bloomberg_excel_functions as abxl
-
 	
 '''
 add_BDS_OPT_CHAIN() EXAMPLE
@@ -27,10 +28,9 @@ Example: add_BDS_OPT_CHAIN(ticker_cell='AAPL US',type_cell='EQUITY', date_overri
 '''
 
 #test the function with both cell references and valid Bloomberg strings
-print('\n')
-print(abxl.add_BDS_OPT_CHAIN(ticker_cell='B2',type_cell='B3', date_override_cell='B6'))
-print(abxl.add_BDS_OPT_CHAIN(ticker_cell='AAPL US',type_cell='EQUITY', date_override_cell='20151231'))
-
+# print('\n')
+# print(abxl.add_BDS_OPT_CHAIN(ticker_cell='B2',type_cell='B3', date_override_cell='B6'))
+# print(abxl.add_BDS_OPT_CHAIN(ticker_cell='AAPL US',type_cell='EQUITY', date_override_cell='20151231'))
 
 '''
 add_BDP_fuction() EXAMPLE
@@ -45,12 +45,10 @@ Example: add_BDP_fuction(security_cell='BBG00673J6L5 Equity', field_cell='SECURI
     	 function arguments are literal string commands excepted by the BDS function
 '''
 #test the function with both cell references and valid Bloomberg strings
-print('\n')
-print(abxl.add_BDP_fuction(security_cell='A3', field_cell='A4'))
-print(abxl.add_BDP_fuction(security_cell='BBG00673J6L5 Equity', field_cell='SECURITY_DES'))
-print(abxl.add_BDP_fuction(security_cell='A3', field_cell='PX_LAST'))
-
-
+# print('\n')
+# print(abxl.add_BDP_fuction(security_cell='A3', field_cell='A4'))
+# print(abxl.add_BDP_fuction(security_cell='BBG00673J6L5 Equity', field_cell='SECURITY_DES'))
+# print(abxl.add_BDP_fuction(security_cell='A3', field_cell='PX_LAST'))
 
 '''
 add_option_BDH() EXAMPLE
@@ -71,13 +69,47 @@ Example: add_option_BDH(security_name='B4', fields=['PX_LAST','PX_BID'], start_d
 		 function argumetns are strings, cell references and lists
 
 '''
-print('\n')
-print(abxl.add_option_BDH(security_name='B4', fields='C3', start_date='A1', end_date='A2', optional_arg = None, optional_val=None))
-print(abxl.add_option_BDH(security_name='B4', fields='C3:C5', start_date='A1', end_date='A2', optional_arg = 'E1:E2', optional_val='F1:F2'))
-print(abxl.add_option_BDH(security_name='IBM US EQUITY', fields='PX_BID, PX_ASK, PX_VOLUME', start_date='20150520', end_date='20170915', optional_arg = 'Days', optional_val='W'))
-print(abxl.add_option_BDH(security_name='B4', fields=['PX_LAST','PX_BID'], start_date='20131230', end_date='A2', optional_arg = 'Days, Fill', optional_val=['W','0']))
+# print('\n')
+# print(abxl.add_option_BDH(security_name='B4', fields='C3', start_date='A1', end_date='A2', optional_arg = None, optional_val=None))
+# print(abxl.add_option_BDH(security_name='B4', fields='C3:C5', start_date='A1', end_date='A2', optional_arg = 'E1:E2', optional_val='F1:F2'))
+# print(abxl.add_option_BDH(security_name='IBM US EQUITY', fields='PX_BID, PX_ASK, PX_VOLUME', start_date='20150520', end_date='20170915', optional_arg = 'Days', optional_val='W'))
+# print(abxl.add_option_BDH(security_name='B4', fields=['PX_LAST','PX_BID'], start_date='20131230', end_date='A2', optional_arg = 'Days, Fill', optional_val=['W','0']))
 
 
+class Test_Bloomberg_Functions(unittest.TestCase):
+
+	def test_add_BDS_OPT_CHAIN(self):
+		self.assertEqual(abxl.add_BDS_OPT_CHAIN(ticker_cell='B2',type_cell='B3', date_override_cell='B6'), 
+			'=BDS(CONCATENATE(B2, " ", B3),"OPT_CHAIN","OPTION_CHAIN_OVERRIDE","M",CONCATENATE("SINGLE_DATE_OVERRIDE=",B6))')
+		self.assertEqual(abxl.add_BDS_OPT_CHAIN(ticker_cell='AAPL US',type_cell='EQUITY', date_override_cell='20151231'),
+			'=BDS(CONCATENATE("AAPL US", " ", "EQUITY"),"OPT_CHAIN","OPTION_CHAIN_OVERRIDE","M",CONCATENATE("SINGLE_DATE_OVERRIDE=","20151231"))')
 
 
+	def test_add_BDP_fuction(self):
+		self.assertEqual(abxl.add_BDP_fuction(security_cell='A3', field_cell='A4'),
+			'=BDP(A3,A4)')
+		self.assertEqual(abxl.add_BDP_fuction(security_cell='BBG00673J6L5 Equity', field_cell='SECURITY_DES'),
+			'=BDP("BBG00673J6L5 Equity","SECURITY_DES")')
+		self.assertEqual(abxl.add_BDP_fuction(security_cell='A3', field_cell='PX_LAST'),
+			'=BDP(A3,"PX_LAST")')
 
+
+	def test_add_option_BDH(self):
+		self.assertEqual(abxl.add_option_BDH(security_name='B4', fields='C3', start_date='A1', end_date='A2', optional_arg=None, optional_val=None),
+			'=BDH(B4,C3,A1,A2)')
+		self.assertEqual(abxl.add_option_BDH(security_name='B4', fields='C3', start_date=dt.datetime(day=5, month=6, year=2013), end_date='A2', optional_arg = None, optional_val=None),
+			'=BDH(B4,C3,20130605,A2)')
+		self.assertEqual(abxl.add_option_BDH(security_name='B4', fields='C3', start_date=dt.date(day=5, month=6, year=2013), end_date='A2', optional_arg = None, optional_val=None),
+			'=BDH(B4,C3,20130605,A2)')
+		self.assertEqual(abxl.add_option_BDH(security_name='B4', fields='C3:C5', start_date='A1', end_date='A2', optional_arg = 'E1:E2', optional_val='F1:F2'),
+			'=BDH(B4,C3:C5,A1,A2,E1:E2,F1:F2)')
+		self.assertEqual(abxl.add_option_BDH(security_name='IBM US EQUITY', fields='PX_BID, PX_ASK, PX_VOLUME', start_date='20150520', end_date='20170915', optional_arg = 'Days', optional_val='W'),
+			'=BDH("IBM US EQUITY","PX_BID, PX_ASK, PX_VOLUME","20150520","20170915","Days","W")')
+		self.assertEqual(abxl.add_option_BDH(security_name='B4', fields=['PX_LAST','PX_BID'], start_date='20131230', end_date='A2', optional_arg = 'Days, Fill', optional_val=['W','0']),
+			'=BDH(B4,"PX_LAST, PX_BID","20131230",A2,"Days, Fill","W, 0")')
+		self.assertRaises(ValueError, abxl.add_option_BDH, security_name='B4', fields='C3', start_date='A1', end_date='A2', optional_arg = 'Fill', optional_val=None)
+		self.assertRaises(ValueError, abxl.add_option_BDH, security_name='B4', fields='C3', start_date='A1', end_date='A2', optional_arg = None, optional_val=0)
+
+
+if __name__ == "__main__":
+	unittest.main()
--- test/test_generate_company_workbooks.py
+++ test/test_generate_company_workbooks.py
@@ -1,16 +1,186 @@
-#imports to get the files
 import os
 import sys
-from test_path import sample_path, target_path, acquirer_path
-parent_path = os.path.abspath(os.pardir)
-path = os.path.join(parent_path,'ma_option_vol')
-
-#adds the file path for the ma_options_vol module to the path that python will search in order to look for modules
+import unittest
+import datetime as dt
+from openpyxl import Workbook, load_workbook
+BASE_DIR = os.path.abspath(os.pardir)
+path = os.path.join(BASE_DIR,'ma_option_vol')
 sys.path.append(path)
-print(path)
-#imports the add_bloomber_excel_functions module
 import generate_company_workbooks as gcw
+from base_test import Test_Base
+
+# c = gcw.Create_Company_Workbooks(source_sheet_name, source_file, target_path, acquirer_path)
+# c.create_company_workbooks()
 
 
-c = gcw.Create_Company_Workbooks(source_file = sample_path, target_path= target_path, acquirer_path = acquirer_path)
-c.create_company_workbooks()
+class Test_Generate_Company_WB(Test_Base):
+	def setUp(self):
+		#Note: note that target_path and acquirer_path are defined in super()
+		self.wb = Workbook()
+		self.sheet_name = 'Filtered Sample Set'
+		self.source_file = 'test_sample.xlsx'
+		self.wb_generator = gcw.Create_Company_Workbooks(self.sheet_name, 
+					self.source_file, self.target_path, self.acquirer_path)
+		
+	def tearDown(self):
+		super().tearDown()
+
+	def test_formated_wb_path_default(self):
+		# formated_wb_path(self, file_name, date_str='', file_extension='xlsx', path=''):
+		file_name = 'test_file'
+		final_file_name = self.wb_generator.formated_wb_path(file_name)
+		self.assertEqual(final_file_name, 'test_file_.xlsx')
+
+	def test_formated_wb_path_kwargs(self):
+		# formated_wb_path(self, file_name, date_str, file_extension='xlsx', path=''):
+		file_name = 'test_file'
+		date_str = '2018-20-06'
+		path = '/Users/username/Documents/'
+		final_file_name = self.wb_generator.formated_wb_path(file_name, date_str, path=path)
+		self.assertEqual(final_file_name, '/Users/username/Documents/test_file_2018-20-06.xlsx')
+
+	def test_save_new_workbook_target(self):
+		file_name = 'target_test'
+		date_str = '2018-20-06'
+		# def save_new_workbook(self,new_workbook,workbook_path, file_name, start_date_str, file_extension='xlsx'):
+		self.wb_generator.save_new_workbook(self.wb, self.target_path, file_name, date_str,)
+		self.assertIn('target', os.listdir(self.test_dir_path))
+		self.assertIn(f'{file_name}_{date_str}.xlsx', os.listdir(self.target_path))
+
+	def test_save_workbook_acquirer(self):
+		file_name = 'acquirer_test'
+		date_str = '2018-20-06'
+		self.wb_generator.save_new_workbook(self.wb, self.acquirer_path, file_name, date_str,)
+		self.assertIn('acquirer', os.listdir(self.test_dir_path))
+		self.assertIn(f'{file_name}_{date_str}.xlsx', os.listdir(self.acquirer_path))
+
+	def test_adjust_to_weekday_saturday(self):
+		#check the calendar, this date is a Saturday
+		saturday = dt.date(year=2018, month=1, day=6)
+		date = self.wb_generator.adjust_to_weekday(saturday)
+		self.assertEqual(date.weekday(), 0)
+
+	def test_adjust_to_weekday_sunday(self):
+		#check the calendar, this date is a Sunday
+		sunday = dt.date(year=2018, month=1, day=7)
+		date = self.wb_generator.adjust_to_weekday(sunday)
+		self.assertEqual(date.weekday(), 0)
+	
+	def test_create_company_workbooks_target(self):	
+		wb = load_workbook(self.source_file)
+		sheet = wb['Filtered Sample Set']
+		for (i, row) in enumerate(sheet.rows):
+			if i != 0:
+				target_name = row[3].value
+				announcement_date = self.wb_generator.adjust_to_weekday(row[1].value.date())
+				new_file = f'{target_name}_{announcement_date}.xlsx'
+				self.wb_generator.new_target_workbook(row, self.target_path)
+				self.assertIn(new_file, os.listdir(self.target_path))
+		
+	def test_create_company_workbooks_acquirer(self):	
+		wb = load_workbook(self.source_file)
+		sheet = wb['Filtered Sample Set']
+		for (i, row) in enumerate(sheet.rows):
+			if i != 0:
+				acquirer_name = row[6].value
+				announcement_date = self.wb_generator.adjust_to_weekday(row[1].value.date())
+				new_file = f'{acquirer_name}_{announcement_date}.xlsx'
+				self.wb_generator.new_acquirer_workbook(row, self.acquirer_path)
+				self.assertIn(new_file, os.listdir(self.acquirer_path))
+
+	def test_company_workbook_data_target(self):
+		#create the workbook
+		wb = load_workbook(self.source_file)
+		sheet = wb['Filtered Sample Set']
+		rows = sheet.rows
+		for i in range(2):
+			rows.__next__()
+		row = rows.__next__()
+		start_date = (row[1].value - dt.timedelta(days=360))
+		data = [
+			('Target Name',row[3].value),
+			('Target Ticker',row[4].value),
+			('Type','Equity'),
+			('Start Date', start_date),
+			('Announcement Date', row[1].value),
+			('End Date', row[2].value),
+			('Formated Start Date', int(start_date.strftime('%Y%m%d'))),
+			('Formated Announcement Date', int(row[1].value.date().strftime('%Y%m%d'))),
+			('Formated End Date', int(row[2].value.strftime('%Y%m%d'))),
+		]
+
+		#create the file from source wb 
+		self.wb_generator.new_target_workbook(row, self.target_path)
+		#load the newly created wb
+		test_wb_file = os.listdir(self.target_path)[0]
+		path = os.path.join(self.target_path, test_wb_file)
+		test_wb = load_workbook(path)
+		ws = test_wb['Options Chain']
+		for row in range(9):
+			for col in range(2):
+				self.assertEqual(ws.cell(column=col+1, row=row+1).value,data[row][col])
+		
+
+	def test_company_workbook_data_acquirer(self):
+		#create the workbook
+		wb = load_workbook(self.source_file)
+		sheet = wb['Filtered Sample Set']
+		rows = sheet.rows
+		for i in range(2):
+			rows.__next__()
+		row = rows.__next__()
+		start_date = (row[1].value - dt.timedelta(days=360))
+		data = [
+			('Acquirer Name',row[6].value),
+			('Acquirer Ticker',row[7].value),
+			('Type','Equity'),
+			('Start Date', start_date),
+			('Announcement Date', row[1].value),
+			('End Date', row[2].value),
+			('Formated Start Date', int(start_date.strftime('%Y%m%d'))),
+			('Formated Announcement Date', int(row[1].value.date().strftime('%Y%m%d'))),
+			('Formated End Date', int(row[2].value.strftime('%Y%m%d'))),
+		]
+
+		#create the file from source wb 
+		self.wb_generator.new_acquirer_workbook(row, self.acquirer_path)
+		#load the newly created wb
+		test_wb_file = os.listdir(self.acquirer_path)[0]
+		path = os.path.join(self.acquirer_path, test_wb_file)
+		test_wb = load_workbook(path)
+		ws = test_wb['Options Chain']
+		for row in range(9):
+			for col in range(2):
+				self.assertEqual(ws.cell(column=col+1, row=row+1).value,data[row][col])
+
+
+	def test_get_company_options_tickers(self):
+		# def get_company_options_tickers(self,reference_sheet, start_date, 
+		#	announcement_date, row, start_column, interval, ticker_cell, type_cell):
+		#test the cells where the add_BDS_OPT_CHAIN function gets added
+		wb = Workbook()
+		st_date = dt.date.today()-dt.timedelta(days=360)
+		ann_date = dt.date.today()
+		row = 1
+		col = 1
+		interval = 30
+		ticker_cell = 'B2'
+		type_cell = 'B3'
+
+		sheet = wb.active
+		self.wb_generator.get_company_options_tickers(sheet, st_date,
+			ann_date, row, col, interval, ticker_cell, type_cell)
+
+		for i in range(1, sheet.max_column +1, 2):
+			self.assertIn( '=BDS(', sheet.cell(row=row, column=i).value)
+
+	def test_create_company_workbooks(self):
+		self.assertEqual(len(os.listdir(self.target_path)), 0)
+		self.assertEqual(len(os.listdir(self.acquirer_path)), 0)
+		self.wb_generator.create_company_workbooks()
+		self.assertGreater(len(os.listdir(self.target_path)), 0)
+		self.assertGreater(len(os.listdir(self.target_path)), 0)
+
+
+if __name__ == "__main__":
+	unittest.main()
